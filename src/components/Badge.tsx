78ce7b4d6d986b203df65aea69376fabdf9dbc9a
sharan nagarajan <60562906+nsharan2000@users.noreply.github.com>
2023-09-26 01:55:47 +0530

    Update Badge.tsx

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -54,14 +54,14 @@ export const Badge = (props: Props) => {
     }}>Powered by
       <a
         ref={liteBadge}
-        href={'https://flowiseai.com'}
+        href={'https://airt.in'}
         target="_blank"
         rel="noopener noreferrer"
         class="lite-badge"
         id="lite-badge"
         style={{ "font-weight": 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Flowise</span>
+        <span> AIRT</span>
       </a>
     </span>
   )
